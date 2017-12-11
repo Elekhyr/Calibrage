c8ea7f23274810ed3325652a02443a2515971b51
Crymsius <romain.m.mars@gmail.com>
2017-12-11 10:56:46 +0100

    add camera calibration... still needs testing though

--- Camera.h
+++ Camera.h
@@ -5,6 +5,7 @@
 #include <opencv2/opencv.hpp>
 #endif
 #include "CameraProperties.h"
+#include "Chessboard.h"
 class Camera
 {
 public:
@@ -13,15 +14,14 @@ public:
 
 	void Init();
 	void SetProperties(const CameraProperties& properties);
-	void Calibrate();
+    void Calibrate(const std::vector<cv::Mat> calibrationImages, Chessboard chessboard, cv::Size boardSize, float edgeLength, cv::Mat& distanceCoefficient);
 	bool Capture();
-
 	const cv::Mat& Snapshot();
 
 private:
 	cv::VideoCapture mCapture;
-
 	cv::Mat mFrame;
 	CameraProperties mProperties;
+    cv::Mat cameraMatrix;
 };
 
--- Chessboard.h
+++ Chessboard.h
@@ -15,10 +15,10 @@ public:
 	void SetProperties(const ChessboardProperties& properties);
 	std::vector<cv::Vec2f> GetCorners(const cv::Mat& image);
 	void DrawCorners(const std::vector<cv::Vec2f>& corners, cv::Mat& image);
+    void CreateKnownCorners();
+    std::vector<cv::Vec2f> mCorners;
 private:
 	ChessboardProperties mProperties;
-	std::vector<cv::Vec2f> mCorners;
 	bool mFound;
-	void CreateKnownCorners();
 };
 
